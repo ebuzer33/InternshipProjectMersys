ef22da2d9956948ac543c098fa92a0771c793439
ahmetDemir63 <86804988+ahmetDemir63@users.noreply.github.com>
2021-07-12 19:30:29 +0300

    formcontent olusturuldu

--- src/test/java/Pages/formContent.java
+++ src/test/java/Pages/formContent.java
@@ -1,6 +1,10 @@
 package Pages;
 
+import Utilities.baseDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class formContent extends parent{
-
-
+    public formContent() {
+        PageFactory.initElements(baseDriver.getDriver(),this);
+    }
 }
