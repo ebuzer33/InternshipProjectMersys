328263889a141b8d80409d803f9a6b47254245bc
ebuzer33 <ebuzer.tutuk@gmail.com>
2021-07-14 23:29:00 +0300

    subjectCategories and subject created and deleted

---
+++ src/test/java/featuresFiles/CreateAndDeleteSubject.feature
@@ -0,0 +1,30 @@
+Feature:
+
+  Background:
+    Given Navigate to website
+    When Enter username and password
+    And Click to login button
+    And Click to accept cookies
+    Then User should be login successfully
+
+  Scenario:Create a Subject
+    And Click to Education
+    And Click on Setup under Education
+    And Click to Subjects
+    And Click to Add Button
+    And Enter Name as "Greek" to Add
+    And Enter Code as "30" to Add
+    And Select a Subject Category as "Foreign Languages" to Add
+    When Click to Save Button
+    Then Success message should be displayed
+
+  Scenario: Delete a Subject
+    And Click to Education
+    And Click on Setup under Education
+    And Click to Subjects
+    And Enter Name as "Greek" to Search
+    And Enter Code as "30" to Search
+    And Select a Subject Categories as "Foreign Languages" to Search
+    And Click to Search Button
+    When Click to Delete Button
+    Then Success message should be displayed
---
+++ src/test/java/stepDefinitions/CreateAndDeleteSubject.java
@@ -0,0 +1,36 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import pages.DialogContent;
+import pages.LeftNav;
+
+public class CreateAndDeleteSubject {
+
+    private  final DialogContent dialogContent;
+    private final LeftNav leftNav;
+
+    public CreateAndDeleteSubject(DialogContent dialogContent, LeftNav leftNav) {
+        this.dialogContent = dialogContent;
+        this.leftNav = leftNav;
+    }
+
+
+    @And("^Click to Subjects$")
+    public void clickToSubjects() {
+        leftNav.findAndClick(leftNav.getSubjects());
+    }
+
+    @And("^Select a Subject Category as \"([^\"]*)\" to Add$")
+    public void selectASubjectCategoryAsToAdd(String subjectCategoryName)  {
+        dialogContent.findAndClick(dialogContent.getSelectAddClick());
+        dialogContent.listSelectOption(dialogContent.getSelectOption(),subjectCategoryName);
+
+    }
+
+    @And("^Select a Subject Categories as \"([^\"]*)\" to Search$")
+    public void selectASubjectCategoriesAsToSearch(String subjectCategoryCode)  {
+        dialogContent.findAndClick(dialogContent.getSelectSearchClick());
+        dialogContent.listSelectOption(dialogContent.getSelectOption(),subjectCategoryCode);
+    }
+}
